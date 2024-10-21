b6c76c9c7c41d9e8415ef0269c68432e09a59a3d
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-20 23:50:52 -0300

    button feito

---
+++ src/components/reciclagem/Button.tsx
@@ -0,0 +1,18 @@
+import { FaArrowRight } from "react-icons/fa6";
+
+type Props = {
+    color: string;
+    text: string;
+}
+
+export const Button = ({ color, text }: Props) => {
+    return (
+        <button
+            style={{ color: `${color}`, borderColor: `${color}` }}
+            className="text-[24px] font-bold border-[2px] rounded-full py-[20px] px-[24px] flex items-center justify-center gap-5"
+        >
+            {text}
+            <FaArrowRight />
+        </button>
+    );
+}
--- src/components/section1/Main.tsx
+++ src/components/section1/Main.tsx
@@ -1,8 +1,11 @@
+import { Button } from "../reciclagem/Button";
+
 export const MainSection1 = () => {
     return (
         <div className="py-2 px-4 mt-96 text-center flex flex-col gap-[24px]">
             <h1 className="text-[40px] leading-[44px] font-bold">Get the Sun to Power Your Home</h1>
-            <p>Viverra viverra nibh enim et aliquam, enim. Tempor, sit mus viverra orci dui consequat turpis scelerisque.</p>
+            <p className="text-[18px] leading-[28.8px]">Viverra viverra nibh enim et aliquam, enim. Tempor, sit mus viverra orci dui consequat turpis scelerisque.</p> 
+            <Button color="#581C87" text="Request a Quote"/>
         </div>
     );
 }
