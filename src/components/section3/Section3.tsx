af1e732b279b2daac0f3b2d06839e31b5e33fd0b
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-22 11:08:57 -0300

    responsividade section3 corrigida

--- src/components/section3/Section3.tsx
+++ src/components/section3/Section3.tsx
@@ -1,22 +1,28 @@
-import Image from "next/image";
-import roxo from "@/app/assets/Rectangle 16.png";
-import cell from "@/app/assets/Mobile-App-Placeholder-1.png";
-import add from "@/app/assets/Frame 4.png"
 import { GridSection } from "../reciclagem/GridSection";
 import { GridItem } from "../reciclagem/Grid";
 import { TitleSection } from "../reciclagem/TitleSection";
+import Image from "next/image";
+import fundoCelular from "@/app/assets/Rectangle 16.png";
+import phoneCelular from "@/app/assets/Phone.png"; 
+import frameCell from "@/app/assets/Frame 4.png";
+import fundoPc from "@/app/assets/Rectangle 16 PC.png";
+import phonePc from "@/app/assets/PhonePc.png";
+import framePc from "@/app/assets/Frame 4 PC.png";
+
 
 export const Section3 = () => {
     return (
-        <section className="flex flex-col-reverse py-12 px-4 md:grid md:grid-cols-2 md:items-center">
-            <div className="mb-32">
-                <Image width={599.56} height={523} alt="roxo" src={roxo} className="absolute z-[-2] left-[-70px] md:w-[400px] lg:w-[600px]" />
-                <div className="">
-                    <Image width={220} height={471.43} alt="cell" src={cell} className="translate-y-[90px] translate-x-[70px] lg:w-[300px] lg:translate-x-[190px]" />
-                    <Image width={110} alt="item4" src={add} className="absolute z-auto translate-y-[-235px] translate-x-[235px] lg:translate-x-[420px] lg:translate-y-[-345px] lg:w-[140px]" />
-                </div>
+        <section className="flex flex-col-reverse py-12 px-4 md:flex-row">
+            <div className="mb-[480px] md:flex-[1] lg:flex-[1]">
+                <Image alt="fundo Celular" src={fundoCelular} className="absolute left-0 lg:hidden"/>
+                <Image alt="phone Celular" src={phoneCelular} className="absolute left-14 lg:hidden"/>
+                <Image alt="frame Celular" src={frameCell} className="absolute transform translate-y-[147px] translate-x-[217px] lg:hidden"/>
+                
+                <Image width={400} alt="fundo pc" src={fundoPc} className="hidden absolute left-0 md:hidden lg:block"/>
+                <Image width={300} alt="phone Pc" src={phonePc} className="hidden absolute left-14 lg:block"/>
+                <Image width={150} alt="frame pc" src={framePc} className="hidden absolute transform translate-y-[180px] translate-x-[260px] lg:block"/>
             </div>
-            <div className="flex flex-col gap-6 mb-16">
+            <div className="flex flex-col gap-6 mb-16 md:flex-[1] lg:flex-[2]">
                 <TitleSection h2="Services" h1="Personalized services" p="Pretium lectus ultrices sit tempor, sit ullamcorper volutpat et et. Auctor turpis semper id sit ornare maecenas lectus sed."/>
                 <GridSection>
                     <GridItem
