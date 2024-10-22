b93201390fb12425e3f227f79fd75eaafc3b5283
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-22 09:46:44 -0300

    inicio da responsividade section3, falta apenas corrigir o tamanho as colunas do grid

--- src/components/reciclagem/Grid.tsx
+++ src/components/reciclagem/Grid.tsx
@@ -5,9 +5,9 @@ type Props = {
 
 export const GridItem = ({h3, p}: Props) => {
     return (
-        <div className="text-center flex flex-col gap-4">
-            <h3 className="font-medium text-[20px] leading-[22px]">{h3}</h3>
-            <p className="text-[16px] leading-[22.4px]">
+        <div className="text-center flex flex-col gap-4 md:text-start">
+            <h3 className="font-medium text-[20px] leading-[22px] md:text-[24px] md:leading-[26.4px]">{h3}</h3>
+            <p className="text-[16px] leading-[22.4px] md:text-[18px] md:leading-[28.8px]">
                 {p}
             </p>
         </div>
--- src/components/section3/Section3.tsx
+++ src/components/section3/Section3.tsx
@@ -7,12 +7,19 @@ import { GridItem } from "../reciclagem/Grid";
 
 export const Section3 = () => {
     return (
-        <section className="py-12 px-4">
+        <section className="flex flex-col-reverse py-12 px-4 md:grid md:grid-cols-2">
+            <div className="mb-32">
+                <Image width={599.56} height={523} alt="roxo" src={roxo} className="absolute z-[-2] left-[-70px] md:w-[400px] lg:w-[600px]" />
+                <div className="">
+                    <Image width={220} height={471.43} alt="cell" src={cell} className="translate-y-[90px] translate-x-[70px] lg:w-[300px] lg:translate-x-[190px]" />
+                    <Image width={110} alt="item4" src={add} className="absolute z-auto translate-y-[-235px] translate-x-[235px] lg:translate-x-[420px] lg:translate-y-[-345px] lg:w-[140px]" />
+                </div>
+            </div>
             <div className="flex flex-col gap-6 mb-16">
-                <div className="text-center flex flex-col gap-6 mb-12">
-                    <h2 className="text-[16px] text-[#D97706] font-medium leading-[17.6px]">Services</h2>
-                    <h1 className="text-[32px] leading-[35.2px] font-bold">Personalized services</h1>
-                    <p className="text-[16px] leading-[22.4px]">
+                <div className="text-center flex flex-col gap-6 mb-12 md:text-start">
+                    <h2 className="text-[16px] text-[#D97706] font-medium leading-[17.6px] md:text-[20px] md:leading-[22px]">Services</h2>
+                    <h1 className="text-[32px] leading-[35.2px] font-bold md:text-[56px] md:leading-[61.6px]">Personalized services</h1>
+                    <p className="text-[16px] leading-[22.4px] md:text-[20px] md:leading-[36px]">
                         Pretium lectus ultrices sit tempor, sit ullamcorper volutpat et et.
                         Auctor turpis semper id sit ornare maecenas lectus sed.
                     </p>
@@ -36,14 +43,7 @@ export const Section3 = () => {
                     />
                 </GridSection>
             </div>
-            <div className="mb-32">
-                <Image width={599.56} height={523} alt="roxo" src={roxo} className="absolute z-[-2] left-[-70px]" />
-                <div className="">
-                    <Image width={220} height={471.43} alt="cell" src={cell} className="translate-y-[90px] translate-x-[70px]" />
-                    <Image width={110} alt="item4" src={add} className="absolute z-auto translate-y-[-235px] translate-x-[235px]" />
-                </div>
 
-            </div>
         </section>
     );
 }
