8e6a2d122859da925dc7ed6a4688635a14a9313a
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-22 12:33:17 -0300

    hover no button adicionado

--- src/components/reciclagem/Button.tsx
+++ src/components/reciclagem/Button.tsx
@@ -6,15 +6,23 @@ type Props = {
 }
 
 export const Button = ({ color, text }: Props) => {
+
+    const setIsHovered = () => {
+
+    }
+
     return (
         <button
-            style={{ color: `${color}`, borderColor: `${color}` }}
-            className="text-[24px] font-bold border-[2px] rounded-full py-[20px] px-[24px] flex items-center justify-center gap-5 md:max-w-[306px] max-h-[64px]"
-        >
-            {text}
-            <div style={{color: `${color}`}}>
-                ➜
-            </div>
+            className={
+                `text-[24px] font-bold border-[2px] rounded-full py-[20px] px-[24px]
+                flex items-center justify-center gap-5 md:max-w-[306px] max-h-[64px]
+                transition-all hover:shadow-2xl
+                ${color === '#581C87' ? `border-[#581C87] text-[#581C87] hover:bg-[#581c87] hover:text-[#FCD34D]` : `border-[#FCD34D] text-[#FCD34D] hover:bg-[#FCD34D] hover:text-[#78350F]`}
+                `
+            } 
+        >   
+            {text} ➜
+            
         </button>
     );
 }
