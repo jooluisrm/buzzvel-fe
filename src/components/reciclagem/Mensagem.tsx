efefd62c88937ba12949efc5d7d2ac8630bf822f
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-23 11:05:11 -0300

    Pequenos ajustes no componente Mensagem

--- src/components/reciclagem/Mensagem.tsx
+++ src/components/reciclagem/Mensagem.tsx
@@ -7,9 +7,9 @@ type Props = {
 
 export const Mensagem = ({ item }: Props) => {
     return (
-        <div className="bg-white p-8 pt-28 mt-16 flex flex-col rounded-lg max-w-[364px] min-w-[343px] transition-all">
+        <div className="bg-white p-8 pt-16 mt-16 flex flex-col justify-between rounded-lg max-w-[364px] min-w-[343px] min-h-[359px] max-h-[442px] transition-all">
             <div>
-                <p className="text-[16px] leading-[22.4px] max-w-[300px] min-h-[202px]">
+                <p className="text-[16px] leading-[22.4px] max-w-[300px] min-h-[202px] md:text-[18px] md:leading-[28.8px] md:pb-5">
                     {item.msg}
                 </p>
             </div>
@@ -18,8 +18,8 @@ export const Mensagem = ({ item }: Props) => {
                     <img width={64} src={`/avatars/${item.avatar}`} alt="avatar" />
                 </div>
                 <div>
-                    <h4 className="text-[16px] leading-[22.4px]">{item.name}</h4>
-                    <p className="text-[#475569] text-[14px] leading-[19.6px]">{item.kwh}</p>
+                    <h4 className="text-[16px] leading-[22.4px] md:text-[18px] md:leading-[28.8px]">{item.name}</h4>
+                    <p className="text-[#475569] text-[14px] leading-[19.6px] md:text-[16px] md:leading-[22.4px]">{item.kwh}</p>
                 </div>
             </div>
         </div>
