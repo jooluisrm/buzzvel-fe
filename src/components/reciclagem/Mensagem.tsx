1e8bcc57800b4e5244b31597c4a9abfe1c6da230
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-23 08:58:20 -0300

    criação do componente Mensagem

---
+++ src/components/reciclagem/Mensagem.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import avatar from "@/app/assets/User Thumb.png"
+
+export const Mensagem = () => {
+    return (
+        <div className="bg-white p-8 pt-28 mt-16 flex flex-col gap-4 rounded-lg">
+            <div>
+                <p className="text-[16px] leading-[22.4px]">
+                    Purus maecenas quis elit eu, aliquet. Tellus porttitor ut sollicitudin sit non fringilla.
+                    Quam nunc volutpat senectus neque eget amet pharetra, euismod. Tempus, nunc, molestie imperdiet
+                    curabitur commodo euismod.
+                </p>
+            </div>
+            <div className="flex items-center gap-2">
+                <div>
+                    <Image alt="avatar" src={avatar}/>
+                </div>
+                <div>
+                    <h4 className="text-[16px] leading-[22.4px]">Jane Cooper</h4>
+                    <p className="text-[#475569] text-[14px] leading-[19.6px]">10KWh</p>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/components/section5/Section5.tsx
+++ src/components/section5/Section5.tsx
@@ -1,10 +1,22 @@
+import { Button } from "../reciclagem/Button";
+import { Mensagem } from "../reciclagem/Mensagem";
 import { TitleSection } from "../reciclagem/TitleSection";
 
 export const Section5 = () => {
     return (
-        <section className="bg-[#581C87] mt-[550px] py-12 px-4">
-            <TitleSection bg="roxo" h1="Make something awesome" h2="Join other Sun harvesters" p="Dui euismod iaculis libero, aliquet vitae et elementum porttitor. Eleifend mi tristique condimentum congue fusce nunc, donec magnis commodo."/>
+        <section className="bg-[#581C87] mt-[550px] py-12 px-4 md:mt-0">
+            <div className="flex flex-col">
+                <TitleSection
+                    bg="roxo"
+                    h1="Make something awesome" h2="Join other Sun harvesters"
+                    p="Dui euismod iaculis libero, aliquet vitae et elementum porttitor. Eleifend mi tristique condimentum congue fusce nunc, donec magnis commodo."
+                />
+                <Button text="Request a Quote" color="#FCD34D" />
+            </div>
 
+            <div>
+                <Mensagem />
+            </div>
         </section>
     );
 }
