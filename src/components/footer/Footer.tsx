8aa34fb62c6d77b2884bc35bde52283d5b204f53
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-25 09:54:25 -0300

    Melhorias do Seo no footer e no layout

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,17 +3,17 @@ import Image from "next/image";
 
 export const Footer = () => {
     return (
-        <footer className="py-6 px-4 flex gap-6 flex-col items-center lg:flex-row lg:justify-between lg:items-center lg:px-20">
+        <footer role="contentinfo" className="py-6 px-4 flex gap-6 flex-col items-center lg:flex-row lg:justify-between lg:items-center lg:px-20">
             <div className="flex flex-col gap-6 lg:flex-row lg:items-center">
-                <Image alt="logo footer" src={logo} className="min-w-[126.81px] min-h-[32px] mx-auto"/>
-                <p className="text-[16px] leading-[22.4px]">@ 2023 Soller, Inc. All rights reserved.</p>
+                <a href="/" aria-label="Pagina inicial"><Image alt="Logo da Soller no rodapé" width={127} height={32} src={logo} className="mx-auto" /></a>
+                <p className="text-[16px] leading-[22.4px]">© {new Date().getFullYear()} Soller, Inc. All rights reserved.</p>
             </div>
             <div>
                 <nav>
                     <ul className="flex gap-6">
-                        <li className="text-[16px] leading-[22.4px]">Terms</li>
-                        <li className="text-[16px] leading-[22.4px]">Privacy</li>
-                        <li className="text-[16px] leading-[22.4px]">Support</li>
+                        <li><a href="/terms" className="text-[16px] leading-[22.4px]">Terms</a></li>
+                        <li><a href="/privacy" className="text-[16px] leading-[22.4px]">Privacy</a></li>
+                        <li><a href="/support" className="text-[16px] leading-[22.4px]">Support</a></li>
                     </ul>
                 </nav>
             </div>
