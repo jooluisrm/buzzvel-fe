9103640f3dab8166e666f98ac46364f8914c6b00
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-25 09:17:36 -0300

    resposividade feita no footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export const Footer = () => {
     return (
-        <footer className="py-6 px-4 flex gap-6 flex-col items-center">
-            <div className="flex flex-col gap-6">
+        <footer className="py-6 px-4 flex gap-6 flex-col items-center lg:flex-row lg:justify-between lg:items-center lg:px-20">
+            <div className="flex flex-col gap-6 lg:flex-row lg:items-center">
                 <Image alt="logo footer" src={logo} className="min-w-[126.81px] min-h-[32px] mx-auto"/>
                 <p className="text-[16px] leading-[22.4px]">@ 2023 Soller, Inc. All rights reserved.</p>
             </div>
@@ -12,8 +12,8 @@ export const Footer = () => {
                 <nav>
                     <ul className="flex gap-6">
                         <li className="text-[16px] leading-[22.4px]">Terms</li>
-                        <li className="text-[16px] leading-[22.4px]">Terms</li>
-                        <li className="text-[16px] leading-[22.4px]">Terms</li>
+                        <li className="text-[16px] leading-[22.4px]">Privacy</li>
+                        <li className="text-[16px] leading-[22.4px]">Support</li>
                     </ul>
                 </nav>
             </div>
