68aea84f3026e6f8d3ea37229231e90713c98059
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-25 09:35:53 -0300

    ajustes na section1

--- src/components/section1/Main.tsx
+++ src/components/section1/Main.tsx
@@ -8,7 +8,10 @@ export const MainSection1 = () => {
             <div className="mt-96 text-center flex flex-col gap-[24px] md:mt-6 md:text-start">
                 <h1 className="text-[40px] leading-[44px] font-bold lg:text-[72px] lg:leading-[79.2px] lg:max-w-[600px]">Get the Sun to Power Your Home</h1>
                 <p className="text-[18px] leading-[28.8px]  max-w-[600px] lg:text-[24px] lg:leading-[38.4px]">Viverra viverra nibh enim et aliquam, enim. Tempor, sit mus viverra orci dui consequat turpis scelerisque.</p>
-                <Button color="#581C87" text="Request a Quote" />
+                <div className="mx-auto md:mx-0">
+                    <Button color="#581C87" text="Request a Quote" />
+                </div>
+
             </div>
             <div className="mt-[64px] flex flex-col gap-[16px]">
                 <p className="text-[16px] leading-[22.4px] max-w-[600px] lg:text-[18px] lg:leading-[28.8px]">
